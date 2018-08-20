60325ec786d67f40e3048bf1232e5f512b1f65e2
VitorWalker <vitor6231@hotmail.com>
2018-08-20 10:49:47 -0300

    javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+let Vitor = prompt("Digite aqui o seu nome:");
+
+console.log(`O nome digitado foi ${Vitor}`);
+
+alert(`Seja bem vindo, ${Vitor}! Aproveite sua visita!`);
+
+if(6 < 7){
+    console.log("PãoDeQueijo");
+
+}
